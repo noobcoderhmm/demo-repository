f8e43b0ab91bb95d972f3382311b018b727a3d61
Test Coder <100346332+noobcoderhmm@users.noreply.github.com>
2025-04-18 17:59:14 +0530

    Update util.go

--- utils/util.go
+++ utils/util.go
@@ -9,6 +9,7 @@ func IsEmailValid(email string) bool {
 	// Very basic regex for illustration
 	re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`)
 	return re.MatchString(email)
+	
 }
 
 // CalculateSum returns the sum of two integers
