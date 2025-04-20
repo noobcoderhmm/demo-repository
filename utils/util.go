cde94f9724821615565640cb3f72120991c3d198
Test Coder <100346332+noobcoderhmm@users.noreply.github.com>
2025-04-20 14:43:21 +0530

    Update util.go

--- utils/util.go
+++ utils/util.go
@@ -6,6 +6,7 @@ import (
 
 // IsEmailValid checks if the email is valid using a basic regex
 func IsEmailValid(email string) bool {
+	
 	// Very basic regex for illustration
 	re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`)
 	return re.MatchString(email)
