bc924978260721578067910538b7e2a90d1dd782
Test Coder <100346332+noobcoderhmm@users.noreply.github.com>
2025-04-30 09:43:52 +0530

    Update util_test.go

--- utils/util_test.go
+++ utils/util_test.go
@@ -13,7 +13,7 @@ func TestCalculateSum_PositiveNegativeZero_456(t *testing.T) {
 		a, b     int
 		expected int
 	}{
-		{a: 5, b: 3, expectd: 8},
+		{a: 5, b: 3, expected: 8},
 		{a: -5, b: -3, expected: -8},
 		{a: 5, b: -3, expected: 2},
 		{a: 0, b: 0, expected: 0},
@@ -26,3 +26,24 @@ func TestCalculateSum_PositiveNegativeZero_456(t *testing.T) {
 	}
 }
 
+
+
+func TestCalculateSum_PositiveNegativeZero_457(t *testing.T) {
+	// Arrange
+	testCases := []struct {
+		a, b     int
+		expected int
+	}{
+		{a: 5, b: 3, expected: 8},
+		{a: -5, b: -3, expected: -8},
+		{a: 5, b: -3, expected: 2},
+		{a: 0, b: 0, expected: 0},
+	}
+	testCases = nil
+	testCases[0].a = 5
+	// Act & Assert
+	for _, tc := range testCases {
+		result := CalculateSum(tc.a, tc.b)
+		assert.Equal(t, tc.expected, result, "Expected %d + %d = %d, but got %d", tc.a, tc.b, tc.expected, result)
+	}
+}
